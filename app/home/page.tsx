4f6df2741e89e6faf7be45f95621cf258bd14fd1
Rebecca Orram <rebecca.ignacio@orram.com.br>
2023-04-13 16:39:33 -0300

    feat: to create a header

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,3 +1,14 @@
+import Image from "next/image";
+import Logo from "../../src/assets/logo.svg";
+import styles from "./styles.module.scss";
+
 export function HomePage() {
-  return <main>Ola mundo</main>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <Image src={Logo} alt="Logo" />
+        <button className={styles.button}> NOVA TRANSAÇÃO </button>
+      </div>
+    </div>
+  );
 }
