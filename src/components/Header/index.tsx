4eed07a832041ec2ae6917d6e0b3965b44db0126
Rebecca Orram <rebecca.ignacio@orram.com.br>
2023-04-18 10:15:21 -0300

    feat: get date to insert

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,20 @@
 "use client";
-import { useState } from "react";
+import { SetStateAction, useState } from "react";
 import Image from "next/image";
 import Logo from "../../../public/assets/logo.svg";
 import Close from "../../../public/assets/close.svg";
 import Income from "../../../public/assets/ENTRADA.svg";
 import Outcome from "../../../public/assets/SAIDA.svg";
 import styles from "./styles.module.scss";
+import { useGlobal } from "@/src/hooks/useGlobal";
 
 export function Header() {
   const [showModal, setShowModal] = useState(false);
+  const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState("");
+  const [type, setType] = useState("");
+  const [category, setCategory] = useState("");
+  const [date, setDate] = useState("");
 
   const handleOpenModal = () => {
     setShowModal(true);
@@ -16,7 +22,47 @@ export function Header() {
 
   const handleCloseModal = () => {
     setShowModal(false);
+    setTitle("");
+    setAmount("");
+    setType("");
+    setCategory("");
+    setDate("");
   };
+
+  const handleNameChange = (event: any) => {
+    setTitle(event.target.value);
+  };
+
+  const handleamountChange = (event: any) => {
+    setAmount(event.target.value);
+  };
+
+  const handleTypeChange = (event: any) => {
+    setType(event.target.innerText.trim());
+  };
+
+  const handleCategoryChange = (event: any) => {
+    setCategory(event.target.value);
+  };
+
+  const handleDateChange = (event: any) => {
+    setDate(event.target.value);
+  };
+
+  const handleSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    const transaction = {
+      title,
+      amount,
+      type,
+      category,
+      date: new Date().toLocaleDateString("pt-BR"),
+    };
+    addTransaction(transaction);
+    handleCloseModal();
+  };
+
+  const { addTransaction } = useGlobal();
 
   return (
     <>
@@ -33,21 +79,43 @@ export function Header() {
                   <Image src={Close} alt="Close" />
                 </button>
                 <h1 className={styles.contentTitle}> Cadatro transação</h1>
-                <form className={styles.form}>
-                  <input type="text" placeholder="Nome" value="" />
-                  <input type="number" placeholder="Preço" value="0" />
+                <form className={styles.form} onSubmit={handleSubmit}>
+                  <input
+                    type="text"
+                    placeholder="Nome"
+                    value={title}
+                    onChange={handleNameChange}
+                  />
+                  <input
+                    type="number"
+                    placeholder="Preço"
+                    value={amount}
+                    onChange={handleamountChange}
+                  />
                   <div className={styles.type}>
-                    <button type="button" className="">
+                    <button
+                      type="button"
+                      className=""
+                      onClick={handleTypeChange}
+                    >
                       <Image src={Income} alt="income" />
                       <span> Entrada </span>
                     </button>
-                    <button type="button" className="">
+                    <button
+                      type="button"
+                      className=""
+                      onClick={handleTypeChange}
+                    >
                       <Image src={Outcome} alt="income" />
                       <span> Saída </span>
                     </button>
                   </div>
-                  <input type="text" placeholder="Categoria" value="" />
-
+                  <input
+                    type="text"
+                    placeholder="Categoria"
+                    value={category}
+                    onChange={handleCategoryChange}
+                  />
                   <button type="submit" className={styles.buttonRegister}>
                     Cadastrar
                   </button>
